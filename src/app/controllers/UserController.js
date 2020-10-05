5706eca1bb174f4634180d301afe853d6ea3a6e0
Caio Felipe <devcaiofelipe15@gmail.com>
2020-10-05 17:01:51 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const { email } = req.body;
+
+    const emailAlreadyExists = await User.findOne({
+      where: { email }
+    });
+    
+    if(emailAlreadyExists) {
+      return res.status(400).json({ error: 'Email already exits' });
+    };
+
+    const user = await User.create(req.body);
+    return res.json(user);
+  };
+};
+
+export default new UserController();
